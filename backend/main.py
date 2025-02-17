85d7a8c2c53180b616b9a7aeba8346e967b04a3d
PAVAN SAI TEJA K <pavankteja@gmail.com>
2025-02-17 13:37:33 +0530

    testing gunicorn

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 from app.config import Config
 from app.routes.auth import auth_bp
@@ -8,6 +8,11 @@ from app.extensions import mongo, jwt
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(auth_bp)
+    
+    @app.route('/')
+    def health_check():
+        return jsonify({"message": "Server is running"}), 200
+    
     CORS(app, resources={r"/*": {"origins": "*"}})
 
     app.config.from_object(Config)
@@ -19,4 +24,4 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
